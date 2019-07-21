1bbbc557abc5af30e390c425fe96018a93232995
skyfall2016 <805747062@qq.com>
2019-07-21 14:24:07 +1000

    day4

---
+++ 709. To Lower Case/toLowerCase.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author: lujianhong
+# @Date:   2019-07-21 14:21:46
+# @Last Modified by:   lujianhong
+# @Last Modified time: 2019-07-21 14:21:52
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        return str.lower()
